d98349415850c5fd13b1b9eb2195301dbbb9146a
Tô Gia Phước Bảo <phuocbao2609@MacBook-Air-cua-To.local>
2024-10-17 01:40:24 -0400

    adding authentication

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
